1a5414cb8a0ff91c40b90c1d27619b7fe2d938a8
Khasbilegt.TS <khasbilegt.ts@gmail.com>
2020-05-21 01:23:30 +0800

    0.2.1

--- tests/test_main.py
+++ tests/test_main.py
@@ -169,4 +169,4 @@ def test_version():
     path = Path(__file__).resolve().parents[1] / "pyproject.toml"
     pyproject = toml.loads(open(str(path)).read())
     return pyproject["tool"]["poetry"]["version"]
-    assert __version__ == "0.1.2"
+    assert __version__ == "0.2.1"
